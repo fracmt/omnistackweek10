35b6a36dd8438a9d7cde18bfc9d1af262aa91b03
Francisco Torres <fracmt@gmail.com>
2020-01-16 18:41:58 +0000

    create controller dev

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
-const axios= require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
@@ -8,32 +7,7 @@ routes.get('/', (req, res) => {
     return res.json({message: "Hello World"});
 });
 
-routes.post('/devs', async (req, res) => {
-    const { github_username, techs, latitude, longitude} = req.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-    //if name no exist, login is 
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-    }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location,
-    });
-
-    return res.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 routes.put('/users/:id', (req, res) => {
     console.log(req.params);
