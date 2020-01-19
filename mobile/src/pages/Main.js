916dac6b2d9e3d907c93b8fb02da6a93c4cfac51
Francisco Torres <fracmt@gmail.com>
2020-01-19 22:21:26 +0000

    Implemented Dev Marker

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,15 +1,85 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text} from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { requestPermissionAsync, getCurrentPositionAsync, requestPermissionsAsync } from 'expo-location';
 
 function Main() {
-    return <MapView style={styles.map} />
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect (() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if(granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+    
+                const { latitude, longitude } = coords;
+    
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                });
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if(!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: 52.669861, longitude: -8.625179 }}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/7252021?s=460&v=4' }} />
+
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Dev's name</Text>
+                        <Text style={styles.devBio}>Dev's Bio: Software Developer</Text>
+                        <Text style={styles.devTechs}>Dev's Techs</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex : 1
     },
+
+    avatar: { 
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF',
+    },
+
+    callout: {
+        width: 200,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
+    }
 });
 
 export default Main;
